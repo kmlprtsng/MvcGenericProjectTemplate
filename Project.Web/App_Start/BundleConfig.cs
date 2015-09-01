ba56dd03efc9f38af83d117317e5aabc4aa763d3
chauhansk <Kamalpreet.chauhan@advancedcomputersoftware.com>
2015-09-01 11:10:13 +0100

    Implemented better look and feel and minimum content

--- Project.Web/App_Start/BundleConfig.cs
+++ Project.Web/App_Start/BundleConfig.cs
@@ -6,9 +6,9 @@ namespace Project.Web
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bootstrap/js").Include("~/js/bootstrap.js", "~/js/site.js"));
-            bundles.Add(new StyleBundle("~/bootstrap/css").Include("~/css/bootstrap.css", "~/css/site.css"));
-
+            bundles.Add(new ScriptBundle("~/bootstrap/js").Include("~/bower_components/bootstrap/dist/js/bootstrap.js", "~/js/site.js"));
+            bundles.Add(new StyleBundle("~/bootstrap/css")
+                .Include("~/bower_components/bootstrap/dist/css/bootstrap.css", "~/css/site.css"));
             BundleTable.EnableOptimizations = true;
         }
     }
