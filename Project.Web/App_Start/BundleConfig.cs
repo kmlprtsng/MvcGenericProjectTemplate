7545dd3045e5241f902c18ee232d6fda1656f837
chauhansk <Kamalpreet.chauhan@advancedcomputersoftware.com>
2015-09-02 17:53:39 +0100

    Add unobtrusive jquery and change password fields to password

--- Project.Web/App_Start/BundleConfig.cs
+++ Project.Web/App_Start/BundleConfig.cs
@@ -7,7 +7,16 @@ namespace Project.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bootstrap/js")
-                .Include("~/bower_components/bootstrap/dist/js/bootstrap.js", "~/js/site.js"));
+                .Include(
+                "~/bower_components/jquery/dist/jquery.js",
+                "~/bower_components/bootstrap/dist/js/bootstrap.js", 
+                "~/js/site.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval")
+                .Include(
+                "~/bower_components/jquery-validation/dist/jquery.validate.js",
+                "~/bower_components/jquery-validation/dist/jquery.validate.unobtrusive.js"));
+
             bundles.Add(new StyleBundle("~/bootstrap/css")
                 .Include("~/bower_components/bootstrap/dist/css/bootstrap.css", "~/css/site.css"));
             BundleTable.EnableOptimizations = true;
