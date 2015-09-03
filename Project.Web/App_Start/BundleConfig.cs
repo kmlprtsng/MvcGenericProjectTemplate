9138360426195d4c23c7c85ffad0981fd4e41f14
chauhansk <Kamalpreet.chauhan@advancedcomputersoftware.com>
2015-09-03 18:03:09 +0100

    Fix jquery validation not working

--- Project.Web/App_Start/BundleConfig.cs
+++ Project.Web/App_Start/BundleConfig.cs
@@ -6,18 +6,16 @@ namespace Project.Web
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bootstrap/js")
+            bundles.Add(new ScriptBundle("~/bundles/js")
                 .Include(
                 "~/bower_components/jquery/dist/jquery.js",
-                "~/bower_components/bootstrap/dist/js/bootstrap.js", 
+                "~/bower_components/bootstrap/dist/js/bootstrap.js",
+                "~/bower_components/jquery-validation/dist/jquery.validate.js",
+                "~/bower_components/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js",
                 "~/js/site.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval")
-                .Include(
-                "~/bower_components/jquery-validation/dist/jquery.validate.js",
-                "~/bower_components/jquery-validation/dist/jquery.validate.unobtrusive.js"));
-
-            bundles.Add(new StyleBundle("~/bootstrap/css")
+            
+            bundles.Add(new StyleBundle("~/bundles/css")
                 .Include("~/bower_components/bootstrap/dist/css/bootstrap.css", "~/css/site.css"));
             BundleTable.EnableOptimizations = true;
         }
