785b673625d6a239ff2f923baccf6338d981cf22
kmlprtsng <kmlprtsng@gmail.com>
2015-09-27 16:22:41 +0100

    Remove project.web2 and create project.web from scratch

---
+++ Project.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Project.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
